a0cce06c033afa94246693d7b807621aa5130a26
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-01 14:31:14 +0100

    search for users feature added

--- github-card/src/components/UserPage.js
+++ github-card/src/components/UserPage.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UserCard from './Card';
 import FollowerCard from './FollowersCard';
+import FindUser from './UserSearch';
 import data from './static';
 import followersData from './staticFollowers';
 
-//const userAPI = 'https://api.github.com/users/theallegrarr';
-//const followersAPI = 'https://api.github.com/users/theallegrarr/followers';
+const userAPI = 'https://api.github.com/users/eliooses';
+const followersAPI = 'https://api.github.com/users/eliooses/followers';
 
 export default class Market extends React.Component {
  constructor(props) {
@@ -14,7 +15,10 @@ export default class Market extends React.Component {
    this.state = {
      user: data,
      followers: followersData,
+     formValue: '',
    };
+
+   // this.update = this.update.bind(this);
  }
  
 //  componentDidMount() {
@@ -28,10 +32,35 @@ export default class Market extends React.Component {
 //        });
 //      });
 //  }
+
+ find = (event) => {
+  const searchUserAPI = `https://api.github.com/users/${this.state.formValue}`;
+  const searchFollowersAPI = `https://api.github.com/users/${this.state.formValue}/followers`;
+
+   const getUser = axios.get(searchUserAPI);
+   const followersPromise = axios.get(searchFollowersAPI);
+   
+   Promise.all([getUser, followersPromise])
+     .then(([userRes, followersRes]) => {
+       this.setState({
+         user: userRes.data,
+         followers: followersRes.data,
+         formValue: '',
+       });
+     });
+ }
+
+ update = (event) => {
+  this.setState({
+    formValue: event.target.value,
+    });
+  }
+
  render() {
    return (
    <>
     <UserCard user={this.state.user}/>
+    <FindUser find={this.find} value={this.state.formValue} update={this.update}/>
     <FollowerCard followers={this.state.followers}/>
    </>
    )
---
+++ github-card/src/components/UserSearch.js
@@ -0,0 +1,13 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+export default function UserSearch (props) {
+
+  return(
+    <div className='searchBar'>
+      <input name='search' type='text' onChange={props.update} value={props.value}></input>
+      <button onClick={props.find}>Find</button>
+    </div>
+  );
+}
